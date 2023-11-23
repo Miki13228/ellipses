494c925b21b5f6f05be08a66e4f4f590498d0fa4
Михаил <0054414@gmail.com>
2023-11-23 17:45:51 +0300

    ellipses

---
+++ main.py
@@ -0,0 +1,49 @@
+import sys
+import random
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def paint(self):
+        self.update()
+        self.btn.clicked.connect(self.paint)
+        self.do_paint = False
+
+    def initUI(self):
+        uic.load("Ui.ui", self)
+        self.btn.clicked.connect(self.paint)
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+        self.do_paint = False
+
+    def draw(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        for i in range(100):
+            x = random.randint(20, 600)
+            y = random.randint(20, 600)
+            r = random.randint(10, 100)
+            qp.drawEllipse(x, y, r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
